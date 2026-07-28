cb20627e70df2549c916ba61541c8d0421a7077f
robertinhocarlos2222 <technerdkm@gmail.com>
2026-07-28 19:55:32 -0300

    Fix: add axios import to index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const axios = require('axios');
 const productsRoutes = require('./routes/products');
 const checkoutRoutes = require('./routes/checkout');
 const webhookRoutes = require('./routes/webhook');
